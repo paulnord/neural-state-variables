f9a817d49eacbd59dea8aa72a9e716284a1a342f
paulnord <paul.nord@valpo.edu>
2022-09-07 10:05:07 -0500

    logs directory addition

--- scripts/encoder_decoder_64_eval_gather.sh
+++ scripts/encoder_decoder_64_eval_gather.sh
@@ -7,7 +7,7 @@ echo "==========================================================================
 echo "============== Evaluating (Gathering) encoder-decoder-64 model on: $dataset (gpu id: $gpu) =============="
 echo "========================================================================================================="
 
-screen -S eval-"$dataset"-64 -dm bash -c "CUDA_VISIBLE_DEVICES="$gpu" python3 ../eval.py ../configs/"$dataset"/model64/config1.yaml ./logs_"$dataset"_encoder-decoder-64_1/lightning_logs/checkpoints NA eval-train NA; \
-                                          CUDA_VISIBLE_DEVICES="$gpu" python3 ../eval.py ../configs/"$dataset"/model64/config2.yaml ./logs_"$dataset"_encoder-decoder-64_2/lightning_logs/checkpoints NA eval-train NA; \
-                                          CUDA_VISIBLE_DEVICES="$gpu" python3 ../eval.py ../configs/"$dataset"/model64/config3.yaml ./logs_"$dataset"_encoder-decoder-64_3/lightning_logs/checkpoints NA eval-train NA; \
+screen -S eval-"$dataset"-64 -dm bash -c "CUDA_VISIBLE_DEVICES="$gpu" python3 ../eval.py ../configs/"$dataset"/model64/config1.yaml ./logs/logs_"$dataset"_encoder-decoder-64_1/lightning_logs/checkpoints NA eval-train NA; \
+                                          CUDA_VISIBLE_DEVICES="$gpu" python3 ../eval.py ../configs/"$dataset"/model64/config2.yaml ./logs/logs_"$dataset"_encoder-decoder-64_2/lightning_logs/checkpoints NA eval-train NA; \
+                                          CUDA_VISIBLE_DEVICES="$gpu" python3 ../eval.py ../configs/"$dataset"/model64/config3.yaml ./logs/logs_"$dataset"_encoder-decoder-64_3/lightning_logs/checkpoints NA eval-train NA; \
                                           exec sh";
--- scripts/encoder_decoder_eval_gather.sh
+++ scripts/encoder_decoder_eval_gather.sh
@@ -7,7 +7,7 @@ echo "==========================================================================
 echo "============== Evaluating (Gathering) encoder-decoder model on: $dataset (gpu id: $gpu) =============="
 echo "======================================================================================================"
 
-screen -S eval-"$dataset" -dm bash -c "CUDA_VISIBLE_DEVICES="$gpu" python3 ../eval.py ../configs/"$dataset"/model/config1.yaml ./logs_"$dataset"_encoder-decoder_1/lightning_logs/checkpoints NA eval-train NA; \
-                                       CUDA_VISIBLE_DEVICES="$gpu" python3 ../eval.py ../configs/"$dataset"/model/config2.yaml ./logs_"$dataset"_encoder-decoder_2/lightning_logs/checkpoints NA eval-train NA; \
-                                       CUDA_VISIBLE_DEVICES="$gpu" python3 ../eval.py ../configs/"$dataset"/model/config3.yaml ./logs_"$dataset"_encoder-decoder_3/lightning_logs/checkpoints NA eval-train NA; \
+screen -S eval-"$dataset" -dm bash -c "CUDA_VISIBLE_DEVICES="$gpu" python3 ../eval.py ../configs/"$dataset"/model/config1.yaml ./logs/logs_"$dataset"_encoder-decoder_1/lightning_logs/checkpoints NA eval-train NA; \
+                                       CUDA_VISIBLE_DEVICES="$gpu" python3 ../eval.py ../configs/"$dataset"/model/config2.yaml ./logs/logs_"$dataset"_encoder-decoder_2/lightning_logs/checkpoints NA eval-train NA; \
+                                       CUDA_VISIBLE_DEVICES="$gpu" python3 ../eval.py ../configs/"$dataset"/model/config3.yaml ./logs/logs_"$dataset"_encoder-decoder_3/lightning_logs/checkpoints NA eval-train NA; \
                                        exec sh";
