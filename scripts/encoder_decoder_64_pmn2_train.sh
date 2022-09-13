ab6e526b3cbb78a533634d18028f9c38e9e58bf8
paulnord <paul.nord@valpo.edu>
2022-09-13 10:37:58 -0500

    added pmn2 model

---
+++ scripts/encoder_decoder_64_pmn2_train.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+dataset=$1
+gpu=$2
+
+echo "==========================================================================================="
+echo "============== Training encoder-decoder-64 model on: $dataset (gpu id: $gpu) =============="
+echo "==========================================================================================="
+
+screen -S train-"$dataset"-64 -dm bash -c "CUDA_VISIBLE_DEVICES="$gpu" python3 ../main.py ../configs/"$dataset"_pmn2/model64/config1.yaml; \
+                                           CUDA_VISIBLE_DEVICES="$gpu" python3 ../main.py ../configs/"$dataset"_pmn2/model64/config2.yaml; \
+                                           CUDA_VISIBLE_DEVICES="$gpu" python3 ../main.py ../configs/"$dataset"_pmn2/model64/config3.yaml; \
+                                           exec sh";
