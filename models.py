e6678b1ad941eb16664a8ccd1abe34dfed511068
paulnord <paul.nord@valpo.edu>
2022-09-13 10:37:29 -0500

    added pmn2 model

--- models.py
+++ models.py
@@ -36,6 +36,44 @@ def mkdir(folder):
         shutil.rmtree(folder)
     os.makedirs(folder)
 
+class VGGPerceptualLossAndMSE(torch.nn.Module):
+    def __init__(self, resize=True):
+        super(VGGPerceptualLossAndMSE, self).__init__()
+        blocks = []
+        blocks.append(torchvision.models.vgg16(pretrained=True).features[:4].eval())
+        blocks.append(torchvision.models.vgg16(pretrained=True).features[4:9].eval())
+        blocks.append(torchvision.models.vgg16(pretrained=True).features[9:16].eval())
+        blocks.append(torchvision.models.vgg16(pretrained=True).features[16:23].eval())
+        for bl in blocks:
+            for p in bl.parameters():
+                p.requires_grad = False
+
+        self.blocks = torch.nn.ModuleList(blocks)
+        self.transform = torch.nn.functional.interpolate
+        self.mean = torch.nn.Parameter(torch.tensor([0.485, 0.456, 0.406], device='cuda').view(1,3,1,1))
+        self.std = torch.nn.Parameter(torch.tensor([0.229, 0.224, 0.225], device='cuda').view(1,3,1,1))
+        self.mseLoss = torch.nn.MSELoss()
+        self.resize = resize
+
+    def forward(self, input, target):
+        if input.shape[1] != 3:
+            input = input.repeat(1, 3, 1, 1)
+            target = target.repeat(1, 3, 1, 1)
+        input = (input-self.mean) / self.std
+        target = (target-self.mean) / self.std
+        if self.resize:
+            input = self.transform(input, mode='bilinear', size=(224, 224), align_corners=False)
+            target = self.transform(target, mode='bilinear', size=(224, 224), align_corners=False)
+        loss = 0.0
+        x = input
+        y = target
+        for block in self.blocks:
+            x = block(x)
+            y = block(y)
+            loss += torch.nn.functional.l1_loss(x, y)
+        loss += self.mseLoss(input,target)
+        return loss
+
 class VGGPerceptualLoss(torch.nn.Module):
     def __init__(self, resize=True):
         super(VGGPerceptualLoss, self).__init__()
@@ -110,6 +148,12 @@ class VisDynamicsModel(pl.LightningModule):
         self.ssim = structural_similarity_index_measure
 
         # model
+        if self.hparams.model_name == 'encoder-decoder-pmn2':
+            self.model = EncoderDecoder(in_channels=3)
+            self.loss_func = VGGPerceptualLossAndMSE(resize=True)
+        if self.hparams.model_name == 'encoder-decoder-64-pmn2':
+            self.model = EncoderDecoder64x1x1(in_channels=3)
+            self.loss_func = VGGPerceptualLossAndMSE(resize=False)
         if self.hparams.model_name == 'encoder-decoder-pmn':
             self.model = EncoderDecoder(in_channels=3)
             self.loss_func = VGGPerceptualLoss(resize=True)
@@ -146,6 +190,10 @@ class VisDynamicsModel(pl.LightningModule):
             output, latent = self.model(x)
         if self.hparams.model_name == 'encoder-decoder-64':
             output, latent = self.model(x, x, False)
+        if self.hparams.model_name == 'encoder-decoder-pmn2' or 'refine' in self.hparams.model_name:
+            output, latent = self.model(x)
+        if self.hparams.model_name == 'encoder-decoder-64-pmn2':
+            output, latent = self.model(x, x, False)
         if self.hparams.model_name == 'encoder-decoder-pmn' or 'refine' in self.hparams.model_name:
             output, latent = self.model(x)
         if self.hparams.model_name == 'encoder-decoder-64-pmn':
@@ -176,6 +224,10 @@ class VisDynamicsModel(pl.LightningModule):
         
         if 'encoder-decoder' in self.hparams.model_name:
             data, target, filepath = batch
+            if self.hparams.model_name == 'encoder-decoder-pmn2':
+                output, latent = self.model(data)
+            if self.hparams.model_name == 'encoder-decoder-64-pmn2':
+                output, latent = self.model(data, data, False)
             if self.hparams.model_name == 'encoder-decoder-pmn':
                 output, latent = self.model(data)
             if self.hparams.model_name == 'encoder-decoder-64-pmn':
